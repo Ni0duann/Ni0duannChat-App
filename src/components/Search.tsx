71b7e9278dfcd1bdb72c37a42d502f37815b10bc
ni0duann <2014567981@qq.com>
2024-10-24 00:53:34 +0800

    feat:add navbar fuction

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { db } from "../firebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState<T>(null);
   const [err, setErr] = useState(false);
+
+  const {currentUser} = useContext(AuthContext)
 
   const handleSearch = async () => {
     const q = query(
@@ -25,8 +28,32 @@ const Search = () => {
   };
 
 
-  const handleSelect = () => {
-    
+  const handleSelect =async () => {
+    // check whether the group(chats in firestore) exists
+    const combinedId = 
+    currentUser.uid > user.uid 
+      ? currentUser.uid + user.uid 
+      : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db,"chats",combinedId));
+      if(!res.exists()){
+        await setDoc(doc(db,"chats",combinedId),{messages:[]})
+        
+        await updateDoc(doc(db,"userChats",currentUser.uid),{
+          [combinedId+".userInfo"] :{
+            uid:currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combinedId+".date"] :serverTimestamp()
+        })
+      }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      setErr(true)
+    }
+    setUser(null)
+    setUsername("")
   }
 
 
@@ -43,6 +70,7 @@ const Search = () => {
           placeholder="Find a user"
           onKeyDown={handleKey}
           onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
       </div>
       {err && <span>User not found!</span>}
